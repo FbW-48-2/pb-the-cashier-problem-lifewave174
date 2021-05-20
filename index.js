7c4161b3c64ef89f625c06d4e61ddc6b7beaf20b
dci <priya.shah174@gmail.com>
2021-05-20 15:42:21 +0200

    finished project, need to do improvements and make the code better

--- index.js
+++ index.js
@@ -7,10 +7,9 @@
 // const euroCoins=[1,2];
 
 
-    euroNotes= [5,10,20,50,100,200,500];
-    euroCents= [1,2,5,10,20,50];
-    euroCoins= [1,2];  
-  
+euroNotes= [5,10,20,50,100,200,500];
+euroCents= [0.01,0.02,0.05,0.10,0.20,0.50];
+euroCoins= [1,2];   
 
 
 //sort all the denominations in descending order
@@ -18,32 +17,63 @@
 euroNotes.sort((a,b)=>b-a);
 euroCents.sort((a,b)=>b-a);
 euroCoins.sort((a,b)=>b-a);
+console.log(euroCents)
 
 //write a function to calculate change
 let bill=3.75;
 let amountPaid=50;
 let change=0;
 let overdueMessage;
-let changeBreakdown=[];
+let changeBreakdown={
+    euroNotes: [],
+    euroCoins: [],
+    euroCents: []
+};
 
-
-//write a function to breakdown change. 
 
 function breakdownChange(){
- if(amountPaid>bill) {
+ if(amountPaid>bill) { //get the change if the change is greater
     change=amountPaid-bill;
-    for(let item of euroNotes) {
+    for(let item of euroNotes) { //check for the notes 
         if((Math.sign(change-item))===0){
-         changeBreakdown.push(item);
+         changeBreakdown["euroNotes"].push(item);
+         break;
        } else if((Math.sign(change-item))===1) {
            change = change-item;
-           changeBreakdown.push(item);
+           changeBreakdown["euroNotes"].push(item);
            if(change>item) {
                change=change-item;
-               changeBreakdown.push(item)
+               changeBreakdown["euroNotes"].push(item)
            }
        }
-    } 
+    }     
+    for(let item of euroCoins)  { //check for the coins
+        if((Math.sign(change-item))===0){
+         changeBreakdown["euroCoins"].push(item);
+         break;
+       } else if((Math.sign(change-item))===1) {
+           change = change-item;
+           changeBreakdown["euroCoins"].push(item);
+           if(change>item) {
+               change=change-item;
+               changeBreakdown["euroCoins"].push(item)
+           }
+       }
+    }    
+    for(let item of euroCents)  { //check for the cents        
+        if((Math.sign(change-item))===0){
+         changeBreakdown["euroCents"].push(item);
+         break;
+       } else if((Math.sign(change-item))===1) {      
+           change = (change-item).toFixed(2);
+           console.log(change)
+           changeBreakdown["euroCents"].push(item);           
+            if(change>item) {
+            change = (change-item).toFixed(2);            
+            changeBreakdown["euroCents"].push(item)
+           }        
+       }
+    }
  } else {
     change=bill-amountPaid;
     overdueMessage= `You buy more than you can afford! You have an overdue amount of -${change}. Pay or leave!`
@@ -54,4 +84,6 @@ function breakdownChange(){
 }
 
 console.log(breakdownChange())
+
+
  
